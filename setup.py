491a9b3544d18afb3be4417fe75b2e65a0e1c3cf
kiorky <kiorky@cryptelium.net>
2009-11-20 09:20:26 +0100

    goodbye setuptools

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setupdir = os.path.abspath(
 os.chdir(setupdir)
 
 name='minitage.recipe.fetch'
-version = '1.40'
+version = '1.41'
 
 def read(*rnames):
     return open(
@@ -50,7 +50,7 @@ setup(
     zip_safe=False,
     install_requires = [
         'zc.buildout',
-        'setuptools',
+        
         'minitage.core',
         'minitage.recipe.common',
         'iniparse',
